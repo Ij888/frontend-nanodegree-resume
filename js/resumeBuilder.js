333312e7da96ebce5437ceb7325e528e862152e0
Ij888 <ij.ofili@gmail.com>
2018-04-19 12:20:17 +0100

    added entry

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+/*eslint-env jquery*/
+/*jslint browser: true*/
+/*global $, jQuery*/
+$("main").append("Paul Wall");
